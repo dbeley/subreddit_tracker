0b83feff8907b47fdb8c9760708a5638d9eb588f
dbeley <6568955+dbeley@users.noreply.github.com>
2022-05-01 17:55:53 +0200

    feat(subreddit_tracker): separate subreddits into several tables

--- subreddit_tracker/db_utils.py
+++ subreddit_tracker/db_utils.py
@@ -8,21 +8,23 @@ class SubredditTrackerDB:
     def __init__(self, db_name="subreddit_tracker.db"):
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
-        self.create_table()
+        self.create_table("measures")
 
-    def create_table(self):
+    def create_table(self, name):
         try:
             self.cursor.execute(
-                """CREATE TABLE IF NOT EXISTS measures (Name text, Date text, Subscribers int, Live_Users int)"""
+                """CREATE TABLE IF NOT EXISTS %s (Name text, Date text, Subscribers int, Live_Users int)"""
+                % name
             )
             self.conn.commit()
         except Exception as e:
             logger.warning(e)
 
     def insert_to_table(self, list_tuples):
-        self.cursor.executemany(
-            "INSERT INTO measures VALUES (?,?,?,?)", list_tuples
-        )
+        for i in list_tuples:
+            subreddit_name = i[0]
+            self.create_table(subreddit_name)
+            self.cursor.execute("INSERT INTO %s VALUES (?,?,?,?)" % subreddit_name, i)
 
     def return_all_rows(self):
         for i in self.cursor.execute("SELECT * FROM measures"):
