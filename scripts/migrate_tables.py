367381c3a96c98bc7e457320f979b4190b5de10b
dbeley <6568955+dbeley@users.noreply.github.com>
2022-05-02 01:29:27 +0200

    fix(scripts): fix db names

--- scripts/migrate_tables.py
+++ scripts/migrate_tables.py
@@ -4,6 +4,6 @@ with open("subreddits_list.txt", "r") as f:
 with open("script.sql", "w") as f:
     for i in subreddits:
         f.write(
-            f"CREATE TABLE IF NOT EXISTS {i} (Name text, Date text, Subscribers int, Live_Users int);\n"
-            f"INSERT INTO {i} SELECT * FROM measures where Name='{i}';\n"
+            f"CREATE TABLE IF NOT EXISTS '{i}' (Name text, Date text, Subscribers int, Live_Users int);\n"
+            f"INSERT INTO '{i}' SELECT * FROM measures where Name='{i}';\n"
         )
